f98438df50313bf43815479792b3da0a4825de3c
vaisakh <mail.vaisakhvm@gmail.com>
2019-06-19 17:58:28 +0530

    Updates

---
+++ precache-manifest.c196410f0d17d975f35241deee3213be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25b5cca84ce0e787b806c6afe5f5f99e",
+    "url": "/resume-builder/index.html"
+  },
+  {
+    "revision": "78eff0a317cef2c43861",
+    "url": "/resume-builder/static/css/main.c288001f.chunk.css"
+  },
+  {
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/resume-builder/static/js/2.7e675ef3.chunk.js"
+  },
+  {
+    "revision": "78eff0a317cef2c43861",
+    "url": "/resume-builder/static/js/main.d14138fc.chunk.js"
+  },
+  {
+    "revision": "fc7ccd356f4ed622e75d",
+    "url": "/resume-builder/static/js/runtime~main.fb5f9dcc.js"
+  }
+]);
